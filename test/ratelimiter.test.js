928395563c9b87808612d0fcba36a161d6edc30a
shogu7 <hugobessaa7@gmail.com>
2025-05-23 12:21:31 +0200

    ratelimiter.test.js fixed

--- test/ratelimiter.test.js
+++ test/ratelimiter.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeAll, beforeEach, afterAll } from 'vitest';
-import { enqueueRequest } from './utils/rateLimiter';
+import { enqueueRequest } from '../utils/ratelimiter';
 
 async function mockSuccessFunction(value) {
   return `Success: ${value}`;
