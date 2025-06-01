6babec5e491925746fe49dca391975d90e1b559d
shogu7 <hugobessaa7@gmail.com>
2025-06-01 21:14:10 +0200

    Fix typo in commandUndefined message

--- utils/error/commandUndefined.js
+++ utils/error/commandUndefined.js
@@ -1,5 +1,5 @@
 async function commandUndefined(message) {
-  await message.reply(`❌ ${message.author}, the command \`${message.content}\` is unknown.`);
+  await message.reply(`${message.author}, the command \`${message.content}\` is unknown. \n Type \`!help\` to see the list of commands.`);
 }
 
 module.exports = { commandUndefined };
