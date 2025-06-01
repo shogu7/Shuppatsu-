af044c2832bee50b032650073856a10b8cb6c7c5
shogu7 <hugobessaa7@gmail.com>
2025-06-01 10:45:30 +0200

    v2

--- utils/scheduler/dailyScheduler.js
+++ utils/scheduler/dailyScheduler.js
@@ -6,7 +6,7 @@ const { sendDailyRelease } = require('./sendDailyRelease');
  */
 function scheduleDailyMessages(client) {
   cron.schedule(
-    '45 10 * * *',
+    '49 10 * * *',
     async () => {
       try {
         const channel = await client.channels.fetch(process.env.CHANNEL_ID_DAILY);
